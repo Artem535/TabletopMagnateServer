b12bbabc2f66bc980e3e822d1918869180e0e13a
Artem Durynin <artem.durynin@raftds.com>
2025-12-08 19:47:57 +0300

    feat: Реализовал возврат правильного типа

---
+++ domain/models/llm_models.py
@@ -0,0 +1,5 @@
+from enum import StrEnum
+
+
+class LLMModels(StrEnum):
+    MODEL_NAME = "TabletopMagnate"
--- domain/service/llm_service.py
+++ domain/service/llm_service.py
@@ -1,12 +1,18 @@
-from tabletopmagnat.types.messages import UserMessage, AiMessage, SystemMessage
+import uuid
 
-from app.settings import Settings
 from tabletopmagnat.service.llm_service import Service
 from tabletopmagnat.types.dialog.dialog import Dialog
+from tabletopmagnat.types.messages import AiMessage, SystemMessage, UserMessage
 from tabletopmagnat.types.messages.base_message import BaseMessage
 
-from domain.exceptions import LLMProcessingError, AppBaseException
-from domain.models.openai import ChatCompletionRequest
+from app.settings import Settings
+from domain.exceptions import AppBaseException, LLMProcessingError
+from domain.models.openai import (
+    ChatCompletionRequest,
+    ChatCompletionResponse,
+    Choice,
+    Message,
+)
 
 
 class LLMService:
@@ -14,22 +20,40 @@ class LLMService:
         self.llm_service: Service = Service(settings.service)
 
     def convert_to_msg(self, content: str, role: str) -> BaseMessage:
-        msg: BaseMessage | None = None
+        mapping = {
+            "user": UserMessage,
+            "assistant": AiMessage,
+            "system": SystemMessage,
+        }
 
-        if role == 'user':
-            msg = UserMessage(content=content)
-        elif role == 'assistant':
-            msg = AiMessage(content=content)
-        elif role == 'system':
-            msg = SystemMessage(content=content)
+        if role not in mapping:
+            raise ValueError(f"Unknown role: {role}")
 
-        return msg
+        res = mapping[role](content=content)
+        return res
 
-    async def run(self, req: ChatCompletionRequest) -> str:
+    async def run(self, req: ChatCompletionRequest) -> ChatCompletionResponse:
         try:
-            dialog = Dialog(messages=[self.convert_to_msg(msg.content, msg.role) for msg in req.messages])
+            dialog = Dialog(
+                messages=[
+                    self.convert_to_msg(msg.content, msg.role) for msg in req.messages
+                ]
+            )
+
             result = await self.llm_service.run(dialog)
         except AppBaseException as e:
             raise LLMProcessingError(str(e))
 
-        return str(result)
+        chat_result = ChatCompletionResponse(
+            id=str(uuid.uuid4()),
+            choices=[
+                Choice(
+                    index=0,
+                    message=Message(role="assistant", content=result),
+                    finish_reason="stop",
+                )
+            ],
+            model=req.model,
+        )
+
+        return chat_result
